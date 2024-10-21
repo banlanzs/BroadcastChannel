c51c0f38c6e3fa7f3676c4c05db5879f5968f72e
banlanzs <147498418+banlanzs@users.noreply.github.com>
2024-10-22 00:08:44 +0800

    banlanzs-Update env.js

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,3 +1,10 @@
-export function getEnv(env, Astro, name) {
-  return env[name] ?? Astro.locals?.runtime?.env?.[name]
+// export function getEnv(env, Astro, name) {
+//   return env[name] ?? Astro.locals?.runtime?.env?.[name]
+// }
+//改
+// src/lib/env.js
+export function getChannels(env, Astro) {
+  const channelEnv = getEnv(env, Astro, 'CHANNEL');
+  return channelEnv ? channelEnv.split(',').map(c => c.trim()) : [];
 }
+
